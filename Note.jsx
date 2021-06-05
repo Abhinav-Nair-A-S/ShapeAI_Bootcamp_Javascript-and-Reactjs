8448b74492bac3dd63947a97274e379aa8fc76f6
Abhinav-Nair-A-S <85395370+Abhinav-Nair-A-S@users.noreply.github.com>
2021-06-05 16:55:02 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and Reactjs</h1>
+      <p1>An amazing bootcamp conducted by ShapeAI taken by Shaurya Sinha.
+        Completely covered the basics of Javascript, HTML and Reactjs
+      </p1>
+    </div>
+  );
+}
+export default Note;
